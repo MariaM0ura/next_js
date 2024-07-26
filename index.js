599cc8c50da891645a89861ae63650bea1b287c1
Maria <mary.m0rhw@gmail.com>
2024-07-26 11:00:03 -0300

    parametros

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ app.get('/', function(req, res){
     res.send('this is a function that will run when the user visits the home page');
 })
 
+app.get('/about/:praOb?', function(req, res){
+    const praOb = req.params.praOb;
+
+    if(!praOb){
+        res.send('<h1>Sem paramentro ')
+    }else{
+        res.send('<h1>O paramentro é  ' + praOb + '</h1>');
+    }
+    
+});
+
+
+
+
 app.listen(4000, function(error){
     if(error){
         console.log('Error running the server');
